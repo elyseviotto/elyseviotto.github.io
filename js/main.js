b9a592440f6ffc328b67d566d4644518b9a1bf9b
Elyse Viotto <elyse.viotto@shopify.com>
2015-12-11 14:56:13 +0000

    temporarily removed the resize function

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@ $(document).ready(function(){
   $('body').jKit();
 
   // Defining a function to set size for #hero
-  function fullscreen(){
-    $('.hero, .hero__container').css({
-      height: $(window).height(),
-      width: $(window).height()
-    });
-  }
+  // function fullscreen(){
+  //   $('.hero, .hero__container').css({
+  //     height: $(window).height(),
+  //     width: $(window).height()
+  //   });
+  // }
 
-  if ( $(window).height() > 320 || $('.hero, .hero__container').width() < 820 ) {
-    fullscreen();
-  }
+  // if ( $(window).height() > 320 || $('.hero, .hero__container').width() < 820 ) {
+  //   fullscreen();
+  // }
 
-  // Window resize
-  $(window).resize(function() {
-    if ( $(window).height() > 320 || $('.hero, .hero__container').width() < 820 ) {
-      fullscreen();
-    }
-  });
+  // // Window resize
+  // $(window).resize(function() {
+  //   if ( $(window).height() > 320 || $('.hero, .hero__container').width() < 820 ) {
+  //     fullscreen();
+  //   }
+  // });
 });
