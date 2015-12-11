80a304465843f3d7eb809224037c24ffb749f323
Elyse Viotto <elyse.viotto@shopify.com>
2015-12-11 04:21:10 +0000

    finalized js effects

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  //parallax effect
+  $('body').jKit();
+
+  // Defining a function to set size for #hero
+  function fullscreen(){
+    $('.hero, .hero__container').css({
+      height: $(window).height(),
+      width: $(window).height()
+    });
+  }
+
+  if ( $(window).height() > 320 || $('.hero, .hero__container').width() < 820 ) {
+    fullscreen();
+  }
+
+  // Window resize
+  $(window).resize(function() {
+    if ( $(window).height() > 320 || $('.hero, .hero__container').width() < 820 ) {
+      fullscreen();
+    }
+  });
+});
